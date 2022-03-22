275f33f71cc4975dec24b61c23a50e353eef865e
luciehdr <houdeyerlucie@hotmail.fr>
2022-03-22 12:56:59 +0100

    rien

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -38,14 +38,25 @@ int check_arg(int argc, char **argv) {
 	return (atoi(argv[1]));
 }
 
-int	set_addrinfo(struct addrinfo hints, struct addrinfo *serveinfo, char *port)
+int	set_addrinfo(struct addrinfo hints, struct addrinfo **serveinfo, char *port)
 {
+	struct sockaddr_in	address;
+	int	status;
+
+	status = 0;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_addr = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM; // for TCP protoctol
 	hints.ai_flags = AI_PASSIVE;
-	if (getaddrinfo(NULL, port, &hints, &serveinfo))
-		return (1);
+	if ((status = getaddrinfo(NULL, port, &hints, serveinfo)))
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		exit (EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(atoi(port)); //  convert values between host and network byte order
+	(*serveinfo)->ai_addr = (struct sockaddr *)&address;
 	return (0);
 }
 
@@ -55,15 +66,13 @@ int	main(int argc, char **argv)
 	int sock_fd, sock, reader;
 	struct addrinfo		hints;
 	struct addrinfo		*serveinfo = NULL;
-	struct sockaddr_in	address;
+
 	int opted = 1;
-	int address_length = sizeof(address);
 	char buffer[1024] = {0};
 	std::string message = "A message from server !";
-
-	if (int_port < 0 || set_addrinfo(hints, serveinfo, argv[1]))
+	if (int_port < 0 || set_addrinfo(hints, &serveinfo, argv[1]))
 		return 1;
-	if (( sock_fd = socket ( AF_INET, SOCK_STREAM, 0)) == 0)
+	if (( sock_fd = socket (serveinfo->ai_family, serveinfo->ai_socktype, serveinfo->ai_protocol)) == 0)
 	{
 		perror ( "Opening of Socket Failed !");
 		exit ( EXIT_FAILURE);
@@ -72,10 +81,7 @@ int	main(int argc, char **argv)
 		perror ( "Can't set the socket" );
 		exit ( EXIT_FAILURE );
 	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( int_port ); //  convert values between host and network byte order
-	if (bind(sock_fd, ( struct sockaddr * )&address, sizeof(address))<0)
+	if (bind(sock_fd, serveinfo->ai_addr, serveinfo->ai_addrlen) <0)
 	{
 		perror ( "Binding of socket failed !" );
 		exit(EXIT_FAILURE);
@@ -85,7 +91,7 @@ int	main(int argc, char **argv)
 		perror ( "Can't listen from the server !");
 		exit(EXIT_FAILURE);
 	}
-	if ((sock = accept(sock_fd, (struct sockaddr *)&address, (socklen_t*)&address_length)) < 0)
+	if ((sock = accept(sock_fd, serveinfo->ai_addr, (socklen_t*)&serveinfo->ai_addrlen)) < 0)
 	{
 		perror("Accept");
 		exit(EXIT_FAILURE);
