0fca1f64ce02bed146bebb6be192c3265fb91ce3
Elise HAUTEFAYE <ehautefa@e2r6p12.clusters.42paris.fr>
2022-06-02 17:07:21 +0200

    [PIKA] did shit

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -33,78 +32,93 @@
 #include "../include/User.hpp"
 #include "../include/Colors.hpp"
 
-#define PASSWORD 	"12345"
-#define BACKLOG 10   // how many pending connections queue will hold
+#define PASSWORD "12345"
+#define BACKLOG 10 // how many pending connections queue will hold
 
-std::string	itoa(int n) {
+std::string itoa(int n)
+{
 	std::stringstream out;
 	out << n;
-	return(out.str());
+	return (out.str());
 }
 
-bool	check_arg(int argc, char **argv) {
-	if (argc != 3) {
+bool check_arg(int argc, char **argv)
+{
+	if (argc != 3)
+	{
 		std::cerr << RED << "Attempt format is \"./ircserv <port> <password>\"" << NC << std::endl;
 		return false;
 	}
-	std::string	port = argv[1];
-	std::string	passWord = argv[2];
-	for (unsigned int i = 0; port[i] != '\0'; i++) {
-		if (!std::isdigit(port[i])) {
+	std::string port = argv[1];
+	std::string passWord = argv[2];
+	for (unsigned int i = 0; port[i] != '\0'; i++)
+	{
+		if (!std::isdigit(port[i]))
+		{
 			std::cerr << RED << "Error : wrong format input" << NC << std::endl;
 			return false;
 		}
 	}
-	if (passWord != PASSWORD) {
+	if (passWord != PASSWORD)
+	{
 		std::cerr << RED << "Error : wrong password input" << NC << std::endl;
 		return false;
 	}
 	return true;
 }
 
-int	init_socket(char *port) {
-	int		sockfd, rv;
-    int 	yes = 1;
-    struct	addrinfo hints, *servinfo, *p;
-	
+int init_socket(char *port)
+{
+	int sockfd, rv;
+	int yes = 1;
+	struct addrinfo hints, *servinfo, *p;
+
 	memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
-        std::cerr << RED << "ERROR: GETADRRINFO failed" << NC << std::endl;
-        return -1;
-    }
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (sockfd == -1) {
-        	std::cerr << RED << "ERROR: SOCKET failed" << NC << std::endl;
-            continue;
-        }
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
-        	std::cerr << RED << "ERROR: SETSOCKOPT failed" << NC << std::endl;
-            return -1;
-        }
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-        	std::cerr << RED << "ERROR: BIND failed" << NC << std::endl;
-            continue;
-        }
-        break;
-    }
-    freeaddrinfo(servinfo);
-    if (p == NULL)  {
-        std::cerr << RED << "ERROR: Failed to bind" << NC << std::endl;
-        return -1;
-    }
-    if (listen(sockfd, BACKLOG) == -1) {
-        std::cerr << RED << "ERROR: LISTEN Failed" << NC << std::endl;
-        return -1;
-    }
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+	if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0)
+	{
+		std::cerr << RED << "ERROR: GETADRRINFO failed" << NC << std::endl;
+		return -1;
+	}
+	for (p = servinfo; p != NULL; p = p->ai_next)
+	{
+		sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (sockfd == -1)
+		{
+			std::cerr << RED << "ERROR: SOCKET failed" << NC << std::endl;
+			continue;
+		}
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1)
+		{
+			std::cerr << RED << "ERROR: SETSOCKOPT failed" << NC << std::endl;
+			return -1;
+		}
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+		{
+			close(sockfd);
+			std::cerr << RED << "ERROR: BIND failed" << NC << std::endl;
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(servinfo);
+	if (p == NULL)
+	{
+		std::cerr << RED << "ERROR: Failed to bind" << NC << std::endl;
+		return -1;
+	}
+	if (listen(sockfd, BACKLOG) == -1)
+	{
+		std::cerr << RED << "ERROR: LISTEN Failed" << NC << std::endl;
+		return -1;
+	}
 	return (sockfd);
 }
 
-std::string	getIfnfo(std::string to_find, std::string buffer) {
+std::string getIfnfo(std::string to_find, std::string buffer)
+{
 	size_t begin = buffer.find(to_find);
 	size_t end = buffer.find("\r\n", begin);
 	std::cout << "begin : " << begin << " end : " << end << std::endl;
@@ -121,40 +135,53 @@ std::string	getIfnfo(std::string to_find, std::string buffer) {
 	return ret;
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-    int							sockfd, new_fd, num_events;
-    struct						sockaddr_storage their_addr; // connector's address information
-    socklen_t					sin_size;
-	struct pollfd 				pfds_tmp;
-	std::vector<struct pollfd>	pfds;
-	
-    if (check_arg(argc, argv) == false || (sockfd = init_socket(argv[1])) < 0)
+	int sockfd, num_events;
+	// struct sockaddr_storage their_addr; // connector's address information
+	// socklen_t sin_size;
+	std::vector<struct pollfd> pfds;
+
+	if (check_arg(argc, argv) == false || (sockfd = init_socket(argv[1])) < 0)
 		return EXIT_FAILURE;
 	std::cout << CYN << "SERVER: waiting for connections..." << NC << std::endl;
-	pfds_tmp.events = POLLIN;
-	pfds_tmp.fd = sockfd;
-	pfds.push_back(pfds_tmp);
-    while (1) {
+	pfds.push_back(pollfd());
+	pfds.back().events = POLLIN;
+	pfds.back().fd = sockfd;
+	while (1)
+	{
 		num_events = poll(&pfds[0], pfds.size(), -1);
 		std::cout << CYN << "SERVER: poll() returned " << num_events << " events" << NC << std::endl;
-		if (num_events == -1) {
+		if (num_events == -1)
+		{
 			std::cout << RED << "ERROR: POLL failed" << NC << std::endl;
-		} else if (pfds[0].revents & POLLIN) {
-			sin_size = sizeof their_addr;
-			new_fd = accept(pfds[0].fd, (struct sockaddr *)&their_addr, &sin_size);
-			if (new_fd == -1)
-				std::cerr << RED << "ERROR: accept failed" << NC << std::endl;
-			pfds_tmp.fd = new_fd;
-			pfds_tmp.events = POLLIN;
-			pfds.push_back(pfds_tmp);
-			char	buffer[512];
-			recv(new_fd, buffer, 512, 0);
-			std::cout << GR << "SERVER: received " << buffer << NC << std::endl;
-			std::cout << GR << "PASSWORD |" << getIfnfo("PASS", buffer) << "|" << NC << std::endl;
-			std::cout << GR << "NICKNAME |" << getIfnfo("NICK", buffer) << "|" << NC << std::endl;
-			std::cout << GR << "USERNAME |" << getIfnfo("USER", buffer) << "|" << NC << std::endl;
 		}
-    }
-    return 0;
+		else if (num_events == 0)
+		{
+			std::cout << RED << "ERROR: TIMEOUT" << NC << std::endl;
+		}
+		else if (pfds[0].revents & POLLIN)
+		{
+			// sin_size = sizeof their_addr;
+			// new_fd = accept(pfds[0].fd, (struct sockaddr *)&their_addr, &sin_size);
+			// if (new_fd == -1)
+			// 	std::cerr << RED << "ERROR: accept failed" << NC << std::endl;
+			// pfds.push_back(pollfd());
+			// pfds.back().events = POLLIN;
+			// pfds.back().fd = new_fd;
+			// char	buffer[512];
+			// recv(new_fd, buffer, 512, 0);
+			// std::cout << GR << "SERVER: received " << buffer << NC << std::endl;
+			char buf[10];
+			ssize_t s = read(pfds[0].fd, buf, sizeof(buf));
+			if (s == -1)
+				std::cout << RED << "ERROR: read failed" << NC << std::endl;
+			printf("    read %zd bytes: %.*s\n",
+				   s, (int)s, buf);
+			// std::cout << GR << "PASSWORD |" << getIfnfo("PASS", buffer) << "|" << NC << std::endl;
+			// std::cout << GR << "NICKNAME |" << getIfnfo("NICK", buffer) << "|" << NC << std::endl;
+			// std::cout << GR << "USERNAME |" << getIfnfo("USER", buffer) << "|" << NC << std::endl;
+		}
+	}
+	return 0;
 }
