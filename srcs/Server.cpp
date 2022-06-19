ff6a9e9627af8f4936ff480e9141729eb03bce8e
LucieHdr <houdeyerlucie@hotmail.fr>
2022-06-19 19:32:30 +0200

    [LUCIE] : I don't understand

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -521,22 +521,26 @@ void	Server::kick(std::vector<User>::iterator user, std::pair<bool, std::string>
 	if (str.first == false)
 		return ;
 	std::cout  << GR << "KICK" << NC << std::endl;
-	std::vector<std::string> tab = split(str.second, ' ');
 	std::map<std::string, Channel>::iterator chan_dest;
 	std::vector<User>::iterator user_dest;
+	std::vector<std::string> tab = split(str.second, ' ');
+	std::cout << tab[0] << std::endl;
+	std::cout << tab[1] << std::endl;
+	std::cout << tab[2] << std::endl;
 	if (tab.size() < 3) {
 		user->send_error(to_string(ERRNEEDMOREPARAMS), "KICK :Not enough parameters");
 		return ;
 	}
+	else if ((chan_dest = this->_channels.find(tab[0])) == this->_channels.end()) {
+		user->send_error(to_string(ERRNOSUCHCHANNEL), tab[0] + " :No such channel");
+		return ;
+	} else if (chan_dest->second.users.find(user->get_fd()) == chan_dest->second.users.end()) {
+		user->send_error(to_string(ERRUSERNOTINCHANNEL), " :User not in channel");
+		return ;
+	}
 	// else if (chan_dest->second.isOperator(user->get_fd()) == false || chan_dest->second.isCreator((user->get_fd())) == false) {
+	// 	std::cout << "HERE SEG 3" << std::endl;
 	// 	user->send_error(to_string(ERRCHANOPRIVSNEED), " :You're not an operator");
-	// 	return ;
-	// }
-	// else if (chan_dest == _channels.end()) {
-	// 	user->send_error(to_string(ERRNOSUCHCHANNEL), " :No such channel");
-	// 	return ;
-	// { else if (chan_dest->second.userIsOn().find(tab[1]) == std::string::npos) {
-	// 	user->send_error(to_string(ERRUSERNOTINCHANNEL), " :User not in channel");
 	// 	return ;
 	// }
 }
